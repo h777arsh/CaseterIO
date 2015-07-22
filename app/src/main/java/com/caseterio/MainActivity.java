362c80cc02da5d6429d3887a5b8ea7eb997939a3
Harshvardhan Trivedi <hjtrivedi777@gmail.com>
2015-07-22 22:50:43 +0530

    just a first test commit

--- app/src/main/java/com/caseterio/MainActivity.java
+++ app/src/main/java/com/caseterio/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    // just test with comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
