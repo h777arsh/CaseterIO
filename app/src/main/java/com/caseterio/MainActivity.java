0a24f48a770c4c1c6685d3aee76543fe8bc48363
Harshvardhan Trivedi <hjtrivedi777@gmail.com>
2015-07-22 23:51:14 +0530

    fixing circle.yml file for circleCI

--- app/src/main/java/com/caseterio/MainActivity.java
+++ app/src/main/java/com/caseterio/MainActivity.java
@@ -1,23 +1,23 @@
 package com.caseterio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     // just test with comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(casterio.com.caseterio.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(casterio.com.caseterio.R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == casterio.com.caseterio.R.id.action_settings) {
+        if (id ==R.id.action_settings) {
             return true;
         }
 
